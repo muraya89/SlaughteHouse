b07c8cfb05339b25649100357b695fc56d80488e
muraya <susanmuraya89@gmail.com>
2022-11-14 14:50:05 +0300

    styling changes

--- admin/admin.php
+++ admin/admin.php
@@ -19,6 +19,7 @@
 		$customers = mysqli_fetch_assoc($db_helpers->countUsers('customer'));
 		$suppliers = mysqli_fetch_assoc($db_helpers->countUsers('supplier'));
 		$most_ordered = $db_helpers->ordering();
+		$onlineUsers = $db_helpers->getOnlineusers('online');
 		
 		$months = ['Jan', 'Feb', 'March', 'April', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 	?>
@@ -54,8 +55,12 @@
 		<div class="item">
 			<table>
 				<tr>
+					<div>
+						<span style="font-size: 20px;">Most Ordered Breeds</span>
+					</div>
+				</tr>
+				<tr>
 					<th>Breed</th>
-					<th>Type</th>
 					<th>Quantity</th>
 				</tr>
 				<?php 
@@ -65,11 +70,83 @@
 				?>
 				<tr>
 					<td><?= $value['breed']; ?></td>
-					<td><?= $value['type']; ?></td>
 					<td><?= $value['sum(quantity)']; ?></td>
 				</tr>
 				<?php endwhile; ?>
 			</table> 
+		</div>
+
+		<div class="item" style="margin-top: 5%;">
+			<table>
+				<tr>
+					<div>
+						<span style="font-size: 20px;">Online Users</span>
+					</div>
+				</tr>				
+				<tr>
+					<th>#</th>
+					<th>Company name</th>
+					<th>Email</th>
+					<th>Phone Number</th>
+					<th>Address</th>
+					<th>
+					<label class="dropdown">
+						<div class="dd-button">Account Type </div><div class="triangle"></div>
+						<input type="checkbox" class="dd-input" id="test">
+						<ul class="dd-menu">
+						<li> <a href="customers_report.php">Customer</a> </li>
+						<li> <a href="suppliers_report.php">Supplier</a> </li>
+						</ul>
+					</label>
+					</th>
+					<th>Date Created</th>
+					<th>
+					<label class="dropdown">
+						<div class="dd-button"> Online Users</div><div class="triangle"></div>
+						<input type="checkbox" class="dd-input" id="test">
+						<ul class="dd-menu">
+						<li> <a href="offline_users.php">Offline Users</a> </li>
+						<hr>
+						<li> <a href="users_report.php">All</a> </li>
+						</ul>
+					</label></th>
+					<th>Action</th>
+				</tr>
+				<?php 
+				// create a while loop to display all the array values and display in a table
+				while($detail = mysqli_fetch_assoc($onlineUsers)) : ?>
+				<tr>
+					<td><?= $detail['id']; ?></td>
+					<td><?= $detail['cname']; ?></td>
+					<td><?= $detail['email']; ?></td>
+					<td><?= $detail['phoneno']; ?></td>
+					<td><?= $detail['address']; ?></td>
+					<td><?= $detail['account']; ?></td>
+					<td><?= $detail['date_created']; ?></td>
+				
+					<td>
+					<?php
+						// check whether the specific user is online/offline and show the respective image
+						if ($detail['status'] == "online") {?>
+						<img src="../public/images/online1.png" alt="" style="height: 20px; width: 20px; margin:auto;">
+					<?php  
+						}else{?>
+						<img src="../public/images/offline.png" alt="" style="height: 20px; width: 20px;margin:auto;">
+					<?php  
+						}
+					?>
+					</td>
+					<td>
+					<form action="AdminClass.php" method="post">
+						<input type="hidden" name="id" value="<?= $detail['id'] ?>"/>
+						<input type="hidden" name="table" value="users" />
+						<input type="hidden" name="redirect_to" value="users_report.php" />
+						<button type="submit" name="deleteSubmit" class="btn">Delete</button>
+					</form>
+					</td>
+				</tr>
+				<?php endwhile; ?>
+			</table>
 		</div>
 	</div>
 </body>
